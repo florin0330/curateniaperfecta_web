cff76e87ccabe85333f603255922b5315b567171
florin0330 <dav123_f@yahoo.com>
2026-08-29 21:44:57 -0400

    Update firebase-config.js

--- public/assets/js/firebase-config.js
+++ public/assets/js/firebase-config.js
@@ -8,12 +8,13 @@
 // functions/index.js (currently "europe-west1").
 
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+  apiKey: "AIzaSyBKSJFmUF8_eonvQY_H4eFIHRBqgxz-QEQ",
+  authDomain: "dfffff-15343.firebaseapp.com",
+  projectId: "dfffff-15343",
+  storageBucket: "dfffff-15343.firebasestorage.app",
+  messagingSenderId: "999499342074",
+  appId: "1:999499342074:web:fb5a21ea8bb95d67ae6943",
+  measurementId: "G-KX5DW7E1Z0",
 };
 
 export const functionsRegion = "europe-west1";
